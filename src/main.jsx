0183697e6bc9621bc647131dbc160307484b29a9
Ncosta09 <ncosta981@gmail.com>
2024-03-12 18:41:08 -0300

    reacomodamiento de rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Rutas from './routes/Rutas.jsx'
+import Rutas from './routes/Rutas.jsx';
+import "../src/assets/styles/main.css"
 // import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
